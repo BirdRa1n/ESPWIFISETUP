059ee02382569fc83e7a49df37383b045678bc6f
Dário Jr <53487868+BirdRa1n@users.noreply.github.com>
2024-08-13 14:12:32 -0300

    Update sketch_espwificonfig.ino

--- sketch_espwificonfig.ino
+++ sketch_espwificonfig.ino
@@ -13,10 +13,10 @@ const int PASS_START_ADDR = 32;
 const int SSID_LENGTH = 32;
 const int PASS_LENGTH = 64;
 const int BUTTON_RESET_PIN = D8;
-const int MAX_WIFI_WAIT_TIME = 20000; // in milliseconds
+const int MAX_WIFI_WAIT_TIME = 20000;  // in milliseconds
 
 const char* AP_SSID = "ESP8266";
-const char* AP_PASS = ""; // Open network
+const char* AP_PASS = "";  // Open network
 
 ESP8266WebServer server(80);
 
@@ -39,12 +39,13 @@ void setup() {
 
   // Initialize EEPROM
   EEPROM.begin(EEPROM_SIZE);
+  setupWebServer();
+  server.begin();
+  delay(2000);
 
   // Check if the reset button is pressed
   if (digitalRead(BUTTON_RESET_PIN) == HIGH) {
     startAccessPoint();
-    setupWebServer();
-    server.begin();
     Serial.println("Access Point started.");
     return;
   }
@@ -56,13 +57,9 @@ void setup() {
   // Attempt to connect to WiFi
   if (connectToWiFi(ssid, pass)) {
     Serial.println("Successfully connected to WiFi.");
-    setupWebServer();
-    server.begin();
   } else {
     Serial.println("Failed to connect to WiFi. Starting Access Point.");
     startAccessPoint();
-    setupWebServer();
-    server.begin();
   }
 }
 
@@ -71,9 +68,8 @@ void loop() {
   server.handleClient();
   if (WiFi.isConnected()) {
     Serial.println("Connected! IP address: " + WiFi.localIP().toString());
-    delay(5000); // Adjust as needed
+    delay(2000);  // Adjust as needed
   } else {
-    restartESP();
   }
 }
 
@@ -93,6 +89,7 @@ bool connectToWiFi(const String& ssid, const String& pass) {
 }
 
 void startAccessPoint() {
+  delay(100);
   WiFi.mode(WIFI_AP);
   WiFi.softAP(AP_SSID, AP_PASS);
   Serial.print("Access Point started. IP address: ");
@@ -102,7 +99,7 @@ void startAccessPoint() {
 void setupWebServer() {
   server.on("/networkinfo", HTTP_GET, handleNetworkInfo);
   server.on("/networks", HTTP_GET, handleNetworkList);
-  server.on("/setting", HTTP_POST, handleSettings);
+  server.on("/setting", HTTP_GET, handleSettings);
   server.on("/", HTTP_GET, handleRoot);
 }
 
@@ -149,7 +146,7 @@ void handleSettings() {
     json["message"] = "WiFi credentials saved. Restarting...";
     server.sendHeader("Access-Control-Allow-Origin", "*");
     server.send(200, "application/json", serializeJsonString(json));
-    delay(5000); // Allow time for message to be sent
+    delay(5000);  // Allow time for message to be sent
     restartESP();
   } else {
     json["message"] = "Invalid credentials.";
@@ -159,18 +156,46 @@ void handleSettings() {
 }
 
 void handleRoot() {
-  DynamicJsonDocument json(256);
+  DynamicJsonDocument json(512);
+
+  // Adicionando informações do ESP8266
   json["msg"] = "ok";
+  json["chip_id"] = ESP.getChipId();
+  json["chip_model"] = "ESP8266";
+  json["flash_size"] = ESP.getFlashChipSize() / (1024 * 1024);  // em MB
+  json["flash_speed"] = ESP.getFlashChipSpeed() / 1000000;      // em MHz
+  json["cpu_freq"] = ESP.getCpuFreqMHz();                       // em MHz
+  json["sdk_version"] = ESP.getSdkVersion();
+  json["free_heap"] = ESP.getFreeHeap();  // Heap livre
+
+  // Adicionando informações da rede Wi-Fi se o dispositivo estiver conectado
+  if (WiFi.status() == WL_CONNECTED) {
+    json["connected"] = true;
+    json["ssid"] = WiFi.SSID();
+    json["rssi"] = WiFi.RSSI();
+    json["ip_address"] = WiFi.localIP().toString();
+    json["subnet_mask"] = WiFi.subnetMask().toString();
+    json["gateway_ip"] = WiFi.gatewayIP().toString();
+  } else {
+    json["connected"] = false;
+    json["ssid"] = "N/A";
+    json["rssi"] = "N/A";
+    json["ip_address"] = "N/A";
+    json["subnet_mask"] = "N/A";
+    json["gateway_ip"] = "N/A";
+  }
+
   String content;
   serializeJson(json, content);
   server.send(200, "application/json", content);
 }
 
+
 String readStringFromEEPROM(int startAddress, int length) {
   String data;
   for (int i = 0; i < length; ++i) {
     char c = EEPROM.read(startAddress + i);
-    if (c == '\0') break; // Stop at null terminator
+    if (c == '\0') break;  // Stop at null terminator
     data += c;
   }
   return data;
@@ -180,7 +205,7 @@ void writeStringToEEPROM(int startAddress, const String& data) {
   for (int i = 0; i < data.length(); ++i) {
     EEPROM.write(startAddress + i, data[i]);
   }
-  EEPROM.write(startAddress + data.length(), '\0'); // Null terminator
+  EEPROM.write(startAddress + data.length(), '\0');  // Null terminator
 }
 
 void restartESP() {
